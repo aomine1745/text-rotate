66670ceb9c5ee148f50a15265860707318756072
aomine <xomarxpl07@outlook.es>
2019-02-04 01:57:40 -0500

    v 1.0

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$('h1').on('mousemove', function(e){
+	centerX = $(this).width()/2;
+	var moveX	= centerX - e.offsetX;
+	$(this).css({
+		'transform' : 'perspective(500px) rotateY(' + moveX/20 + 'deg)'
+	})
+})
